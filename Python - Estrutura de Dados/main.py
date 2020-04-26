7335597c1add6d972dcf17c20870caa2b3765a84
AlissonSantos17 <alisson.flima@souunit.com.br>
2020-04-26 19:58:15 -0300

    Fila e pilha

--- Python - Estrutura de Dados/main.py
+++ Python - Estrutura de Dados/main.py
@@ -1,11 +1,15 @@
 from vetores import vetor
 from listas import lista_ligada
 from listas import lista_duplamente_ligada
+from pilhas import pilha
+from filas import fila
 
 print(30 * "-", "MENU", 30 * "-")
 print('1 - Vetores\n'
       '2 - Listas Ligadas\n'
-      '3 - Listas Duplamente Ligadas\n')
+      '3 - Listas Duplamente Ligadas\n'
+      '4 - Pilhas\n'
+      '5 - Filas\n')
 menu = int(input('Digite a opção desejada: '))
 
 if menu == 1:
@@ -41,6 +45,23 @@ elif menu == 3:
     lista_teste.inserir(5)
     print(lista_teste.inserirPosicao(2, 10))
     print(lista_teste)
-    # lista_teste.removerElemento(4)
     lista_teste.removerPosicao(1)
     print(lista_teste)
+
+elif menu == 4:
+    pilha_teste = pilha.Pilha()
+    pilha_teste.empilhar(5)
+    print(pilha_teste.desempilhar())
+
+elif menu == 5:
+    fila_teste = fila.Fila()
+    fila_teste.enfileirar(1)
+    fila_teste.enfileirar(2)
+    fila_teste.enfileirar(3)
+    fila_teste.enfileirar(4)
+    print(fila_teste)
+    print(fila_teste.desenfileirar())
+    print(fila_teste)
+
+else:
+    print('Opção invalida')
